9384954cbdf16ee9b7009a6d52a42419b037ac6d
Kigo-P <peterkigo33@gmail.com>
2024-09-03 21:12:19 +0300

    used objects inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,13 @@
-from vehicle import Vehicle
+from vehicle import Vehicle# have to import first
 
-class Car(Vehicle):
+class Car(Vehicle):# subclass- inherits from the superclass/parent
+    # overwriting the go function from the superclass/parent
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
+
+# toyota = Car(14, 4)
+# print(toyota.wheel_size) # The Car has access to the parent/super class
+# print(toyota.wheel_number)
+# # The go method will overwrite the one from the superclass
+# print(toyota.go())# VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!! instead of  "vrrrrrrrooom!"
